a74b688ec29847a3854da44a7b1d729f992ac69b
vividsystem <notrealbutnotobot@gmail.com>
2023-04-11 14:56:01 +0200

    fix(nvim): added config for null-ls

---
+++ nvim/.config/nvim/lua/plugins/null-ls.lua
@@ -0,0 +1,22 @@
+local nls = require('null-ls')
+local bi = nls.builtins
+
+local sources = {
+  bi.formatting.prettierd,
+  bi.formatting.shfmt,
+  bi.formatting.fixjson,
+  bi.formatting.black.with { extra_args = { "--fast" } },
+  bi.formatting.isort,
+
+  bi.diagnostics.write_good,
+  bi.diagnostics.eslint_d,
+  bi.diagnostics.tsc,
+
+  bi.code_actions.gitsigns,
+  bi.code_actions.gitrebase,
+  bi.code_actions.refactoring,
+}
+
+nls.setup({
+  sources = sources
+})
