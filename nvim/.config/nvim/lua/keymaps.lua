93bfd6d62964d7749f896fd485af23115b1cfce0
vividsystem <notrealbutnotobot@gmail.com>
2023-04-14 22:25:27 +0200

    feat(nvim): added which-key support using desc

--- nvim/.config/nvim/lua/keymaps.lua
+++ nvim/.config/nvim/lua/keymaps.lua
@@ -5,6 +5,12 @@ local default_opts = {
   silent = true
 }
 
+local add_desc = function(desc)
+  local opts = default_opts
+  opts['desc'] = desc
+  return opts
+end
+
 km.set('n', '<C-f>', '/')
 
 km.set('n', '<Up>', '<Nop>', default_opts)
@@ -12,10 +18,10 @@ km.set('n', '<Down>', '<Nop>', default_opts)
 km.set('n', '<Left>', '<Nop>', default_opts)
 km.set('n', '<Right>', '<Nop>', default_opts)
 
-km.set('n', '<C-J>', ':bprev<CR>', default_opts) --> fix interference with nvimtree
-km.set('n', '<C-K>', ':bnext<CR>', default_opts)
+km.set('n', '<C-J>', ':bprev<CR>', add_desc('change to next buffer')) --> fix interference with nvimtree
+km.set('n', '<C-K>', ':bnext<CR>', add_desc('change to previous buffer'))
 
-km.set('i', "jk", "<ESC>", default_opts)
-km.set('t', 'jk', '<C-\\><C-n>', default_opts)
+km.set('i', "jk", "<ESC>", add_desc('ESC'))
+km.set('t', 'jk', '<C-\\><C-n>', add_desc('ESC'))
 
 
